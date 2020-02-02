e71dceafaf41c3f66ab7ac072751e3e1adf34824
Tristan <tristanw3@outlook.com>
2020-02-02 22:54:02 +1100

    added obj data storage

--- frontend/src/Components/FormulaBar/FormulaBar.js
+++ frontend/src/Components/FormulaBar/FormulaBar.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "./FormulaBar.scss";
 
-
 export default class FormulaBar extends React.Component {
 
   render() {
     return (
       <div className="FormulaBar">
-        <i class="material-icons">create</i>
+        <i className="material-icons">create</i>
         <input
           value={this.props.currentCellValue}
           onChange={this.props.updateCurrentCellValue}
--- frontend/src/Components/Sheet/Sheet.js
+++ frontend/src/Components/Sheet/Sheet.js
@@ -17,6 +17,21 @@ export default class Sheet extends React.Component {
       [0, 70, 1, 2, 3],
       [0, 80, 1, 2, 3]
     ],
+    cellsData2: {
+      "0_0": {
+        value: 22
+      },
+      "2_2": {
+        value: 10
+      },
+      "1_3": {
+        value: 34
+      },
+      "4_5": {
+        value: 11
+      }
+
+    },
     selectedCell: {
       row: 0,
       col: 0
@@ -27,6 +42,13 @@ export default class Sheet extends React.Component {
     this.setState({
       ...this.state,
       cellsData: changeValue(e.target.value, this.state.cellsData, this.state.selectedCell.row, this.state.selectedCell.col),
+    });
+  };
+
+  updateCurrentCellValue = e => {
+    this.setState({
+      ...this.state,
+      cellsData2: changeValue2(e.target.value, this.state.cellsData2, this.state.selectedCell.row, this.state.selectedCell.col)
     });
   };
 
@@ -41,19 +63,50 @@ export default class Sheet extends React.Component {
   }
 
 
+  cellFunc = (rowIndex) => {
+    let rowData = [];
+    let y;
+    for (let cInd = 0; cInd < this.state.size.rows; cInd++) {
+      let selectedCell = false;
+      if (
+        rowIndex === this.state.selectedCell.row &&
+        cInd === this.state.selectedCell.col
+      ) {
+        selectedCell = true;
+      }
+
+      let valCell = 0;
+      if (this.state.cellsData2.hasOwnProperty(rowIndex + "_" + cInd)) {
+        valCell = this.state.cellsData2[rowIndex + "_" + cInd].value
+      }
+
+      y = (<Cell
+        key={rowIndex + "-" + cInd}
+        row={rowIndex}
+        col={cInd}
+        val={valCell}
+        selected={selectedCell}
+        updateCurrentCellValue={this.updateCurrentCellValue}
+        changeSelected={this.changeSelected}
+      />)
+      rowData.push(y)
+    }
+    return rowData;
+  };
+
+
   render() {
-    let selCellVal = getRawFormula(this.state.cellsData, this.state.selectedCell.row, this.state.selectedCell.col)
+    // let selCellVal = getRawFormula(this.state.cellsData, this.state.selectedCell.row, this.state.selectedCell.col)
+    let selCellVal = getRawFormula2(this.state.cellsData2, this.state.selectedCell.row, this.state.selectedCell.col)
 
     let cellFunc = (row, rowIndex) => {
       return row.map((cellValue, colIndex) => {
-        let selectedCell;
+        let selectedCell = false;
         if (
           rowIndex === this.state.selectedCell.row &&
           colIndex === this.state.selectedCell.col
         ) {
           selectedCell = true;
-        } else {
-          selectedCell = false;
         }
         return (
           <Cell
@@ -72,7 +125,8 @@ export default class Sheet extends React.Component {
     let rowsAndCells = this.state.cellsData.map((rowEle, rowIndex) => {
       return (
         <div className="rowStyling" key={rowIndex}>
-          {cellFunc(rowEle, rowIndex)}
+          {/* {cellFunc(rowEle, rowIndex)} */}
+          {this.cellFunc(rowIndex)}
         </div>
       );
     });
@@ -109,7 +163,24 @@ function getRawFormula(cells, row, col) {
   return cells[row][col]
 }
 
+function getRawFormula2(cells, row, col) {
+  if (cells.hasOwnProperty([row + "_" + col])) {
+    return cells[row + "_" + col].value
+  }
+
+  return 0
+}
+
 function changeValue(val, cells, row, col) {
   cells[row][col] = val;
+  return cells
+}
+
+function changeValue2(val, cells, row, col) {
+  cells[row + "_" + col] = {
+    value: val
+  }
+
+
   return cells
 }
