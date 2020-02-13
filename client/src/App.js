ec48ca46487e8b7c73b82479824823ced47e78ec
Tristan Warren <tristanw3@outlook.com>
2020-02-13 23:25:35 +1100

    commits

--- client/src/App.js
+++ client/src/App.js
@@ -10,11 +10,22 @@ import Login from "./Login/Login";
 import Signup from "./SignUp/SignUp";
 
 export default class App extends React.Component {
+  state = {
+    user: "id"
+  };
+  componentDidMount() {
+    let auth = JSON.parse(sessionStorage.getItem("auth"));
+    if (!auth) return;
+    this.setState({
+      user_id: auth.user._id
+    });
+  }
+
   render() {
     return (
       <Router>
         <Spreadsheet path="/" />
-        <SheetsPage path="/Sheets" />
+        <SheetsPage path="/sheets" user_id={this.state.user_id} />
         <Login path="/login" />
         <Signup path="/signup" />
       </Router>
--- client/src/SheetsPage/SheetsPage.js
+++ client/src/SheetsPage/SheetsPage.js
@@ -1,18 +1,46 @@
 import React from "react";
 import "../App.scss";
 
+import axios from "axios";
+
 import Footer from "../Components/Footer/Footer";
 
 export default class SheetsPage extends React.Component {
   state = {
+    sheetsItems: [],
     currentFont: "Lucida Sans Unicode",
     currentFontSize: 11
   };
 
+  addSheet() {
+    console.log("go");
+    axios
+      .post(
+        `/sheets/create`,
+        {
+          title: "blank",
+          userId: this.props.userId
+        },
+        {
+          // headers: { Authorization: `Bearer ${auth.token}` }
+        }
+      )
+      .then(response => {
+        this.setState({
+          ...this.state,
+          sheetsItems: response.data
+        });
+        console.log(this.state.foodItems);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
   render() {
     return (
       <div className="App">
-        text info
+        <button onclick={this.addSheet()}>New Sheet</button>
         <Footer />
       </div>
     );
