9ba5aee3f2436d353bea5ae16f9e542c4235264d
Tristan Warren <tristanw3@outlook.com>
2020-02-13 19:09:33 +1100

    new commit

--- src/server.js
+++ src/server.js
@@ -10,7 +10,6 @@ const sheetRouter = require("./routes/sheetRoutes");
 const cellRouter = require("./routes/cellRoutes");
 
 const app = express();
-const PORT = process.env.PORT || 8080;
 
 // Convert incoming json to javascript object
 app.use(express.static(path.join(__dirname, "client", "build")));
@@ -19,10 +18,14 @@ app.use(userRouter);
 app.use(sheetRouter);
 app.use(cellRouter);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log("Server is up on " + PORT);
 });
