c3db411bbb18d6a16f4632d9359f152aa1ddbcf5
Tristan Warren <tristanw3@outlook.com>
2020-02-13 19:58:55 +1100

    things

--- src/db/connect.js
+++ src/db/connect.js
@@ -2,7 +2,16 @@ const mongoose = require("mongoose");
 
 const dbName = "spreadsheet-app";
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/" + dbName, {
-  useNewUrlParser: true,
-  useCreateIndex: true
-});
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/" + dbName, {
+//   useNewUrlParser: true,
+//   useCreateIndex: true
+// });
+mongoose.connect(
+  "mongodb+srv://tristanw2:securetest@cluster0-id0nc.mongodb.net/" +
+    dbName +
+    "?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true
+  }
+);
--- src/server.js
+++ src/server.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log("Server is up on " + PORT);
 });
