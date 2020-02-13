16336f46ee54268f0c5034cc6d768ab915c44d6c
Tristan Warren <tristanw3@outlook.com>
2020-02-13 20:15:46 +1100

    deploy

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const cellRouter = require("./routes/cellRoutes");
 const app = express();
 
 // Convert incoming json to javascript object
-app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 app.use(express.json());
 app.use(userRouter);
 app.use(sheetRouter);
@@ -21,7 +21,7 @@ app.use(cellRouter);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
   });
 }
 
